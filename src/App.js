b2653f559a1b987587da62d42d4164d8cdcf4e51
Lucky <luckykashyap3430@gmail.com>
2023-06-25 13:29:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { APP_ID, SERVER_SECRET } from './constants';
 
-function App() {
+
+export default function App() {
+      const roomID ="CodeWithLucky";
+      let myMeeting = async (element) => {
+     // generate Kit Token
+      const appID = APP_ID;
+      const serverSecret = SERVER_SECRET;
+      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(),"Lucky");
+
+
+     // Create instance object from Kit Token.
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      // start the call
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: 'Personal link',
+            url:
+             window.location.protocol + '//' + 
+             window.location.host + window.location.pathname +
+              '?roomID=' +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        },
+      });
+
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
   );
 }
-
-export default App;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const APP_ID = 2094771425;
+
+export const SERVER_SECRET = 'c718fef4a6ce6a369266e3f3bf0fce2c';
+
+
